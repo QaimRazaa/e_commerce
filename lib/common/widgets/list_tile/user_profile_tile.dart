664e856d0739e85f0c709b3bcff812680e223d14
QaimRazaa <qaimr320@gmail.com>
2025-10-17 20:35:54 +0500

    settings screen

---
+++ lib/common/widgets/list_tile/user_profile_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../images/circular_image.dart';
+
+class UserProfileTile extends StatelessWidget {
+  const UserProfileTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircularImage(
+        image: AppImages.user,
+        width: 50,
+        height: 50,
+      ),
+      title: Text('Syed Qaim Raza', style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColors.white),),
+      subtitle: Text('qaimr320@gmail.com', style: Theme.of(context).textTheme.bodyMedium!.apply(color: AppColors.white),),
+      trailing: IconButton(onPressed: (){}, icon: Icon(Iconsax.edit, color: AppColors.white,)),
+    );
+  }
+}
--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,10 +1,15 @@
 import 'package:e_commerce/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:e_commerce/common/widgets/images/circular_image.dart';
+import 'package:e_commerce/common/widgets/list_tile/settings_menu_tile.dart';
+import 'package:e_commerce/common/widgets/text/section_heading.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../common/widgets/list_tile/user_profile_tile.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -26,10 +31,86 @@ class SettingsScreen extends StatelessWidget {
                       ).textTheme.headlineMedium!.apply(color: AppColors.white),
                     ),
                   ),
-                  SizedBox(height: Sizes.spaceBetweenSections,),
-                  ListTile(
-                    leading: CircularImage(image: AppImages.user),
-                  )
+                  UserProfileTile(),
+                  SizedBox(height: Sizes.spaceBetweenSections),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(Sizes.defaultSpace),
+              child: Column(
+                children: [
+                  SectionHeading(title: 'Account Settings'),
+                  SizedBox(height: Sizes.spaceBetweenItems),
+                  SettingsMenuTile(
+                    icon: Iconsax.safe_home,
+                    title: 'My Address',
+                    subTitle: 'Set shopping address',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.shopping_cart,
+                    title: 'My Cart',
+                    subTitle: 'Add, remove products and move to checkout',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.bag_tick,
+                    title: 'My Orders',
+                    subTitle: 'In-progress and completed Orders',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.bank,
+                    title: 'Bank Account',
+                    subTitle: 'Withdraw balance to registered bank account',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.discount_shape,
+                    title: 'My Coupons',
+                    subTitle: 'List of all the discounted coupons',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.notification,
+                    title: 'Notifications',
+                    subTitle: 'Set any kind of Notification message',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.security_card,
+                    title: 'Account Privacy',
+                    subTitle: 'Manage data usage and connected accounts',
+                  ),
+                  SizedBox(height: Sizes.spaceBetweenSections),
+                  SectionHeading(title: 'App Settings'),
+                  SizedBox(height: Sizes.spaceBetweenItems),
+                  SettingsMenuTile(
+                    icon: Iconsax.document_upload,
+                    title: 'Load Data',
+                    subTitle: 'Upload Data to your cloud Firebase',
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.location,
+                    title: 'GeoLocation',
+                    subTitle: 'Set recommendation based on your Location',
+                    trailing: Switch(value: true, onChanged: (value) {}),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.security_user,
+                    title: 'Safe Mode ',
+                    subTitle: 'Search result is safe for all ages',
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.image,
+                    title: 'HD quality image',
+                    subTitle: 'Set image quality',
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                  ),
+                  SizedBox(height: Sizes.spaceBetweenSections),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text('Logout'),
+                    ),
+                  ),
                 ],
               ),
             ),
